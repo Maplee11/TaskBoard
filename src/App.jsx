b9809a554f05de9a5b2a364183755b9f88ad4ae4
Maple <2281683038@qq.com>
2024-07-05 14:52:15 +0800

    auto save and show banner

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,9 @@ import TaskBoard from "./components/TaskBoard.jsx";
 import axios from 'axios';
 
 function App() {
-    const [message, setMessage] = useState('');
-
-    useEffect(() => {
-        axios.get('http://192.168.190.1:7001/api/hello')
-            .then(response => {
-                setMessage(response.data.message);
-                console.log(`${message} is received`);
-            })
-            .catch(error => {
-                console.error('There was an error fetching the data!', error);
-            });
-    }, []);
-
     return (
       <>
           <h1 className="text-7xl text-indigo-400 font-bold">Task Manager</h1>
-          <h2>{message}</h2>
           <div>
               <TaskBoard />
           </div>
--- src/components/TaskBoard.jsx
+++ src/components/TaskBoard.jsx
@@ -1,7 +1,10 @@
-import React, { useState, useRef } from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import Modal from 'react-modal';
 import GetUsrInput from "./GetUsrInput.jsx";
 import { useDrag, useDrop } from 'react-dnd';
+import axios from 'axios';
+
+const client = axios.default;
 
 Modal.setAppElement('#root');
 
@@ -9,41 +12,89 @@ const ItemTypes = {
     TASK: 'task',
 };
 
+const backendUrl = "http://192.168.190.1:7001";
+
 const TaskBoard = () => {
     const [editMode, setEditMode] = useState(false);
+    const [taskList, setTask] = useState({todo: [], undergoing: [], done: []});
+    const [createBackupBanner, setCreateBackupBanner] = useState(false);
+    const [loadBackupBanner, setLoadBackupBanner] = useState(false);
+    const [isInitialized, setInitialize] = useState(false);
+
+
+    const getExampleTaskList = () => {
+        client.get(backendUrl + "/task/getExample").then((response) => {
+            setTask(response.data);
+        })
+    }
+
+    const getUsrTaskList = () => {
+        client.get(backendUrl + "/task/getUsrTaskList").then((response) => {
+            setTask(response.data);
+        })
+    }
+
+    const saveUsrTask = () => {
+        client.post(backendUrl + "/task/save", taskList);
+    }
+
+    const clearAllBanner = () => {
+        setCreateBackupBanner(false);
+        setLoadBackupBanner(false);
+    }
+
+    const createBackup = () => {
+        clearAllBanner();
+        setCreateBackupBanner(true);
+        setTimeout(() => setCreateBackupBanner(false), 3000);
+    }
+
+    const loadBackup = () => {
+        clearAllBanner();
+        setLoadBackupBanner(true);
+        setTimeout(() => setLoadBackupBanner(false), 3000);
+    }
+
+    useEffect(() => {
+        if(isInitialized === false){
+            setInitialize(true);
+            getUsrTaskList()
+        }
+        else{
+            saveUsrTask();
+        }
+    }, [taskList]);
 
     return (
         <div>
+            {createBackupBanner && (
+                <div className="fixed top-0 left-0 w-full bg-green-500 text-white py-2 px-4 shadow-lg flex justify-center items-center z-50">
+                    备份已创建
+                </div>
+            )}
+            {loadBackupBanner && (
+                <div className="fixed top-0 left-0 w-full bg-fuchsia-400 text-white py-2 px-4 shadow-lg flex justify-center items-center z-50">
+                    备份已加载
+                </div>
+            )}
             <button
                 onClick={() => setEditMode(!editMode)}
                 className="mt-4 px-4 py-2 bg-blue-400 text-white rounded"
             >
-                {editMode ? "退出编辑" : "编辑"}
+                {editMode ? "退出编辑" : "编辑模式"}
             </button>
-            <TasksArea editMode={editMode} />
+            <button onClick={createBackup} className="mt-4 px-4 py-2 bg-green-500 text-white rounded">
+                创建备份
+            </button>
+            <button onClick={loadBackup} className="mt-4 px-4 py-2 bg-fuchsia-400 text-white rounded">
+                加载备份
+            </button>
+            <TasksArea editMode={editMode} setTask={setTask} taskList={taskList}/>
         </div>
     );
 }
 
-const TasksArea = ({ editMode }) => {
-    const [taskList, setTask] = useState({
-        todo: [
-            '规划本周的工作目标',
-            '完成项目提案的草稿',
-            '安排与客户的会议',
-        ],
-        undergoing: [
-            '更新项目进度报告',
-            '学习新的编程语言',
-            '完成期末编程作业',
-        ],
-        done: [
-            '购买办公耗材',
-            '整理电子邮件收件箱',
-            '准备团队建设活动',
-        ],
-    });
-
+const TasksArea = ({editMode, setTask, taskList}) => {
     const [newTask, setNewTask] = useState("");
     const [modalIsOpen, setModalIsOpen] = useState(false);
     const modalResolve = useRef(null);
@@ -59,7 +110,7 @@ const TasksArea = ({ editMode }) => {
         setModalIsOpen(false);
     };
 
-    const save2newtask = () => {
+    const save2newTask = () => {
         if(newTask !== "" && modalResolve.current)
             modalResolve.current(newTask);
     }
@@ -79,7 +130,6 @@ const TasksArea = ({ editMode }) => {
             ...prevTasks,
             [listName]: prevTasks[listName].filter((_, index) => index !== taskIndex),
         }));
-        console.log(`Task ${listName}[${taskIndex}] deleted`);
     };
 
     const moveTask = (fromList, toList, taskIndex) => {
@@ -95,9 +145,12 @@ const TasksArea = ({ editMode }) => {
 
     return (
         <div className="flex h-screen p-4 space-x-4">
-            <Column taskList={taskList} title="未开始" listName="todo"       addTaskHandler={addTask} editMode={editMode} deleteTaskHandler={deleteTask} moveTaskHandler={moveTask} />
-            <Column taskList={taskList} title="执行中" listName="undergoing" addTaskHandler={addTask} editMode={editMode} deleteTaskHandler={deleteTask} moveTaskHandler={moveTask} />
-            <Column taskList={taskList} title="已完成" listName="done"       addTaskHandler={addTask} editMode={editMode} deleteTaskHandler={deleteTask} moveTaskHandler={moveTask} />
+            <Column taskList={taskList} title="未开始" listName="todo" addTaskHandler={addTask} editMode={editMode}
+                    deleteTaskHandler={deleteTask} moveTaskHandler={moveTask}/>
+            <Column taskList={taskList} title="执行中" listName="undergoing" addTaskHandler={addTask}
+                    editMode={editMode} deleteTaskHandler={deleteTask} moveTaskHandler={moveTask}/>
+            <Column taskList={taskList} title="已完成" listName="done" addTaskHandler={addTask} editMode={editMode}
+                    deleteTaskHandler={deleteTask} moveTaskHandler={moveTask}/>
 
             <Modal
                 isOpen={modalIsOpen}
@@ -113,13 +166,13 @@ const TasksArea = ({ editMode }) => {
                 }}
             >
                 <div className="flex flex-wrap justify-center">
-                    <GetUsrInput setUsrInput={setNewTask} />
-                    <br />
+                    <GetUsrInput setUsrInput={setNewTask}/>
+                    <br/>
                     <button onClick={closeModal} className="mt-4 px-4 py-2 bg-red-500 text-white rounded">
                         取消
                     </button>
                     <button onClick={() => {
-                        save2newtask();
+                        save2newTask();
                         closeModal();
                     }} className="mt-4 px-4 py-2 bg-green-500 text-white rounded">
                         保存
@@ -130,7 +183,7 @@ const TasksArea = ({ editMode }) => {
     );
 };
 
-const Column = ({ taskList, title, listName, addTaskHandler, editMode, deleteTaskHandler, moveTaskHandler }) => {
+const Column = ({taskList, title, listName, addTaskHandler, editMode, deleteTaskHandler, moveTaskHandler }) => {
     const [, drop] = useDrop({
         accept: ItemTypes.TASK,
         drop: (item, monitor) => {
@@ -147,10 +200,11 @@ const Column = ({ taskList, title, listName, addTaskHandler, editMode, deleteTas
 
     return (
         <div ref={drop} className="bg-gray-100 rounded-lg shadow p-4 inline-block min-w-max flex-grow">
-            <h2 className="w-full bg-red-500 font-bold text-1xl text-white py-2 rounded-lg">{title}</h2>
+            <h2 className="mb-1.5 w-full bg-red-500 font-bold text-1xl text-white py-2 rounded-lg px-36">{title}</h2>
             <div className="space-y-2">
                 {taskList[listName].map((taskName, index) => (
-                    <TaskCard key={index} taskName={taskName} editMode={editMode} listName={listName} index={index} deleteTaskHandler={() => deleteTaskHandler(listName, index)} />
+                    <TaskCard key={index} taskName={taskName} editMode={editMode} listName={listName} index={index}
+                              deleteTaskHandler={() => deleteTaskHandler(listName, index)}/>
                 ))}
                 <button
                     className="w-full bg-green-500 text-white py-2 rounded-lg"
@@ -164,7 +218,7 @@ const Column = ({ taskList, title, listName, addTaskHandler, editMode, deleteTas
 };
 
 
-const TaskCard = ({ taskName, editMode, listName, index, deleteTaskHandler }) => {
+const TaskCard = ({taskName, editMode, listName, index, deleteTaskHandler}) => {
     const [{ isDragging }, drag] = useDrag({
         type: ItemTypes.TASK,
         item: { taskName, listName, index },
